49ddeac8ecf66c0442ec5221fa43273413b6aef7
snrKostik <loljkwowk@gmail.com>
2025-07-26 00:15:34 +0700

    Просто все закоммитил #3

---
+++ src/main/java/snr/kostik/BirchBot.java
@@ -0,0 +1,23 @@
+package snr.kostik;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import snr.kostik.events.*;
+import snr.kostik.lists.EmoteList;
+
+import javax.security.auth.login.LoginException;
+
+public class BirchBot {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder jdaBuilder = JDABuilder.createDefault(Secret.discord_token);
+
+        jdaBuilder
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_EXPRESSIONS)
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setActivity(Activity.playing("В ЗБД"))
+                .addEventListeners(new ReadyEventListener(), new MessageEventListener(), new EmoteList())
+                .build();
+    }
+}

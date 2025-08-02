d406bfd3e5ba12b541f924343f29ea55435de3bf
snrKostik <loljkwowk@gmail.com>
2025-08-02 21:10:06 +0700

    Сменил статус, сделав Обжекта счастливее
    Вместо "Играет в в збд" теперь "Играет в Зону Березовых Домов"

--- src/main/java/snr/kostik/BirchBot.java
+++ src/main/java/snr/kostik/BirchBot.java
@@ -1,5 +1,6 @@
 package snr.kostik;
 
+import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -7,17 +8,18 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import snr.kostik.events.*;
 import snr.kostik.lists.EmoteList;
 
-import javax.security.auth.login.LoginException;
-
 public class BirchBot {
-    public static void main(String[] args) throws LoginException {
-        JDABuilder jdaBuilder = JDABuilder.createDefault(Secret.discord_token);
+  public static void main(String[] args) throws LoginException {
+    JDABuilder jdaBuilder = JDABuilder.createDefault(Secret.discord_token);
 
-        jdaBuilder
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_EXPRESSIONS)
-                .setStatus(OnlineStatus.DO_NOT_DISTURB)
-                .setActivity(Activity.playing("Зону Березовых Домов"))
-                .addEventListeners(new ReadyEventListener(), new MessageEventListener(), new EmoteList())
-                .build();
-    }
+    jdaBuilder
+        .enableIntents(GatewayIntent.MESSAGE_CONTENT,
+                       GatewayIntent.GUILD_MESSAGES,
+                       GatewayIntent.GUILD_EXPRESSIONS)
+        .setStatus(OnlineStatus.DO_NOT_DISTURB)
+        .setActivity(Activity.playing("Зону Березовых Домов"))
+        .addEventListeners(new ReadyEventListener(), new MessageEventListener(),
+                           new EmoteList())
+        .build();
+  }
 }

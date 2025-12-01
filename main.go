d289e22fd064817c74d79e9591b0ae176cae51b6
snrKostik <loljkwowk@gmail.com>
2025-12-02 00:11:26 +0700

    rewrited main features of java bot to golang

--- main.go
+++ main.go
@@ -7,41 +7,42 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/snrKostik/birch-bot/handlers"
+	_ "github.com/snrKostik/birch-bot/handlers"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	dg, err := discordgo.New("BOT " + token)
-	if err != nil {
-		fmt.Println("cant create new session", err)
-		return
-	}
-	dg.AddHandler(messageCreate)
-	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildEmojis | discordgo.IntentsMessageContent
+	token := goEnvVar("DISCORD_BOT_TOKEN")
+	appID := goEnvVar("DISCORD_APP_ID")
 
-	err = dg.Open()
+	dg := initBot(token)
+
+	dg.AddHandler(handlers.MessageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildEmojis | discordgo.IntentsMessageContent
+	handlers.ChangeStatus(dg, appID)
+
+	open(dg)
+
+	fmt.Println("bot is fully working!")
+
+	close(dg)
+}
+
+func open(dg *discordgo.Session) {
+	err := dg.Open()
 	if err != nil {
-		fmt.Println("cant create a connection", err)
+		fmt.Println("cant create connection: ", err)
 		return
 	}
-	fmt.Println("bot is fully working!")
+}
+
+func close(dg *discordgo.Session) {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
 	dg.Close()
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	if m.Content == "!wl" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "test")
-		if err != nil {
-			fmt.Println("cant send message", err)
-			return
-		}
-	}
 }
